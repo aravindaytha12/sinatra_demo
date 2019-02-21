9166c315a96cb8509227262f2b37b7dd0fc9ef5f
Aravind Aytha <aravind.aytha@cognizant.com>
2019-02-21 04:35:56 -0500

    #Added new file

--- app.rb
+++ app.rb
@@ -11,4 +11,12 @@ class SampleClass < Sinatra::Base
   get '/xyz' do
     "Am in xyz route"
   end
+
+  get '/index' do
+    erb :index
+  end
+
+  get '/students/index' do
+    erb :'students/index', :locals => {:arr => ["1", "2"]}
+  end
 end
